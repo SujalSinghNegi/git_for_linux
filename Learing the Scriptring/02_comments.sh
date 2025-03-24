fc978e07191d3b7c8b63c3a6802f870738873c5a
sujal singh negi <negisujal2007@gmail.com>
2025-03-24 21:49:20 +0530

    starting to learn the scripting in linux from basic

---
+++ Learing the Scriptring/02_comments.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "helo"
+
+#comment
+
+<<comment
+
+this
+is multi line 
+commetns
+
+comment
+
+<<as
+
+this as is the custom made multi  line comments, 
+this means <<as we have write for starting the multiline comment and
+for ending this we have to write the same word as to end this
+
+as
+
+echo "heloooo"
+
